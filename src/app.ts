1c97545e30e9ef68ddace32eb875ed825c4d2841
Matheus Andrade <matheuslincon50@gmail.com>
2024-09-25 16:11:35 +1000

    refactor: global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,22 @@
 import fastify from 'fastify'
+import { usersRoutes } from './http/controllers/users/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
+
+app.register(usersRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation error.', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  }
+
+  return reply.status(500).send({ message: 'Internal server error.' })
+})
