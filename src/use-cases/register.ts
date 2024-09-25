20fdce14506bea2d4024f10b497bb1d34f569522
Matheus Andrade <matheuslincon50@gmail.com>
2024-09-25 16:09:52 +1000

    feat: register use-case

---
+++ src/use-cases/register.ts
@@ -0,0 +1,33 @@
+import { UserRepository } from '@/repositories/user-repository'
+import { hash } from 'bcryptjs'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
+
+interface RegisterUseCaseRequest {
+  name: string
+  email: string
+  password: string
+}
+
+export class RegisterUseCase {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const password_hash = await hash(password, 6)
+
+    const userWithSameEmail = await this.userRepository.findByEmail(email)
+
+    if (userWithSameEmail) {
+      throw new UserAlreadyExistsError()
+    }
+
+    const user = await this.userRepository.create({
+      name,
+      email,
+      password_hash,
+    })
+
+    return {
+      user,
+    }
+  }
+}
